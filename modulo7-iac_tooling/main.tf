2e95620be2e0932899ba0c3c59c23a601814c14f
Guilherme Henrique Cavaco <37093203+gui-awk@users.noreply.github.com>
2025-11-04 21:26:27 -0300

    feature: ajustado para usar vpc-modulo9 e alterado nome do sg que estava em uso

--- modulo7-iac_tooling/main.tf
+++ modulo7-iac_tooling/main.tf
@@ -11,81 +11,40 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "azurerm_resource_group" "main" {
-  name     = "jewelry-app-rg"
-  location = "us-east-1"
+# --------- REDE ---------
+data "aws_vpc" "main" {
+  id = "vpc-06786ee7f7a163059"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "jewelry-vpc"
+data "aws_subnets" "all" {
+  filter {
+    name   = "vpc-id"
+    values = ["vpc-06786ee7f7a163059"]
   }
 }
 
-resource "aws_subnet" "internal" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "internal"
-  }
+data "aws_subnet" "internal" {
+  id = tolist(data.aws_subnets.all.ids)[0]
 }
-
 
 resource "aws_eip" "main" {
   domain = "vpc"
-
-  tags = {
-    Name = "jewelry-pip"
-  }
+  tags = { Name = "jewelry-pip" }
 }
 
-
-resource "aws_security_group" "main" {
+data "aws_security_group" "main" {
   name   = "jewelry-nsg"
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description      = "HTTP 8080"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "jewelry-nsg"
-  }
+  vpc_id = "vpc-06786ee7f7a163059"
 }
-
-
 
 resource "aws_network_interface" "main" {
-  subnet_id = aws_subnet.internal.id
+  subnet_id = data.aws_subnet.internal.id
+  tags = { Name = "jewelry-nic" }
+}
 
-  tags = {
-    Name = "jewelry-nic"
-  }
+resource "aws_network_interface_sg_attachment" "main" {
+  security_group_id    = data.aws_security_group.main.id
+  network_interface_id = aws_network_interface.main.id
 }
 
 resource "aws_eip_association" "main" {
@@ -93,42 +52,44 @@ resource "aws_eip_association" "main" {
   network_interface_id = aws_network_interface.main.id
 }
 
-
-resource "aws_network_interface_sg_attachment" "main" {
-  security_group_id    = aws_security_group.main.id
-  network_interface_id = aws_network_interface.main.id
-}
-
+# --------- AMI + CHAVE ---------
 data "aws_ami" "ubuntu_2004" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
-
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 }
 
-resource "aws_key_pair" "adminuser" {
-  key_name   = "adminuser"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
-
-data "aws_ami" "ubuntu_2004" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+resource "null_resource" "generate_ssh_key" {
+  provisioner "local-exec" {
+    command = <<EOT
+      if [ ! -f ~/.ssh/id_rsa.pub ]; then
+        mkdir -p ~/.ssh
+        ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ""
+      fi
+    EOT
   }
 }
 
-resource "aws_key_pair" "adminuser" {
-  key_name   = "adminuser"
-  public_key = file("~/.ssh/id_rsa.pub")
+resource "tls_private_key" "adminuser" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
 }
 
+resource "aws_key_pair" "adminuser" {
+  key_name   = "adminuser"
+  public_key = tls_private_key.adminuser.public_key_openssh
+}
+
+resource "local_file" "adminuser_privkey" {
+  content         = tls_private_key.adminuser.private_key_pem
+  filename        = pathexpand("~/.ssh/adminuser.pem")
+  file_permission = "0600"
+}
+
+# --------- EC2 ---------
 resource "aws_instance" "jewelry_vm" {
   ami           = data.aws_ami.ubuntu_2004.id
   instance_type = "t2.micro"
@@ -153,16 +114,15 @@ resource "aws_instance" "jewelry_vm" {
     rm -rf proway-docker/
     git clone https://github.com/gui-awk/proway-docker.git
     cd proway-docker/modulo7-iac_tooling
-    
+
     docker build -t jewelry-app .
     docker run -d -p 8080:80 jewelry-app
   EOF
 
-  tags = {
-    Name = "jewelry-vm"
-  }
+  tags = { Name = "jewelry-vm" }
 }
 
+# --------- OUTPUTS ---------
 output "vm_public_ip" {
   value = aws_eip.main.public_ip
 }
